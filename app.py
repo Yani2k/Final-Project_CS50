46649d4c4f8dee7c8d5550c7e4d1e5b32890e02c
Yani2k <yaniyankovenchev@gmail.com>
2023-03-11 19:52:27 +0200

    updating the READ.me

--- app.py
+++ app.py
@@ -21,7 +21,7 @@ app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 app.config["SECRET_KEY"] = config("SECRET_KEY")
 Session(app)
-socketio = SocketIO(app, manage_session=False)
+socketio = SocketIO(app, manage_session=False, cors_allowed_origins='*')
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 #configuring mail settings
